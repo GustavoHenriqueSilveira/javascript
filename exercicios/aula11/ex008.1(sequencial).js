15080f0de57a324b44c99987551581512fea416c
GustavoHenriqueSilveira <ghms.7000@gmail.com>
2022-04-04 22:27:50 -0300

    aula11

---
+++ exercicios/aula11/ex008.1(sequencial).js
@@ -0,0 +1,8 @@
+console.log('O console funcionou corretamente!')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
